9cf776b7cdd572252acb95f8206848a3c3a0606a
michaelv <michaelv@comda.co.il>
2023-08-23 17:53:37 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './common/user';
 import { HttpService } from './services/http.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,23 +14,36 @@ export class AppComponent {
 
   json$ = this.httpService.json$;
 
+  test$ = this.httpService.test$;
+
   users: User[] = [];
-  user: User | undefined;
+  user: User;
 
+  constructor(private httpService: HttpService, http: HttpClient) {
+    this.user = new User('bbb', 25);
 
-
-  constructor(private httpService: HttpService) {
-    // this.user = new User('bbb', 25);
+    // http.get<User[]>('/weatherforecast').subscribe(
+    //   (result) => {
+    //     this.users = result;
+    //   },
+    //   (error) => console.error(error)
+    // );
   }
 
   ngOnInit() {
     //this.users = this.httpService.getUsers();
 
-    //  this.user = new User('aaa', 23);
+    this.user = new User('aa3a', 23);
+    this.users.push(new User('vbvnb', 234));
+    this.users.push(new User('v44nb', 34));
     // this.http.get('assets/user.json').subscribe({next: data:any) => this.user = new User(data.name, data.age)});
     // this.http.get('assets/user.json').subscribe({next:(data:any) => this.user = new User(data.name, data.age)});
     // this.httpService.getUsers().subscribe({next:(data: User[]) => this.users=data});
-    this.httpService.getUsers().subscribe({next:(data: User[]) => this.users.push(new User("eee", 45))});
+    this.httpService.getUsers().subscribe({
+      next: (data: User[]) => this.users.push(new User('eee', 45)),
+    });
+    this.test$ = this.httpService.test$;
+    console.log('qwXXn eqe');
 
     // this.httpService.getData().subscribe({
     //   next: (data: any) => {
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -5,31 +5,46 @@ import { User } from '../common/user';
 
 type Post = {
   body: string;
-  title: string
-}
+  title: string;
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  urlPost: string = 'https://jsonplaceholder.typicode.com/posts/2';
+  companyPost: string = 'https://localhost:7116/managementapi/companies/test';
 
-  json$ = this.http.get<Post>('https://jsonplaceholder.typicode.com/posts/2').pipe(
+  json$ = this.http.get<Post>(this.urlPost).pipe(
     map(val => ({title: val.title, body: val.body})),
     share()
-);
+  );
 
-  getData(){
-    return this.http.get('assets/user.json')
+  // json$ = this.http.get(this.urlPost);
+
+  public aa = this.http.get(this.urlPost);
+
+  test$ = this.http.get<Post>(this.companyPost).pipe(
+    map((val) => ({ title: val.title, body: val.body })),
+    share()
+  );
+
+    // test$ = this.http.get(this.companyPost);
+
+  getData() {
+    return this.http.get('assets/user.json');
   }
 
-  getUsers() : Observable<User[]> {
-    return this.http.get('assets/users.json').pipe(map((data:any)=>{
-        let usersList = data["userList"];
-        return usersList.map(function(user: any): User {
-            return new User(user.userName, user.userAge);
-          });
-    }));
-}
+  getUsers(): Observable<User[]> {
+    return this.http.get('assets/users.json').pipe(
+      map((data: any) => {
+        let usersList = data['userList'];
+        return usersList.map(function (user: any): User {
+          return new User(user.userName, user.userAge);
+        });
+      })
+    );
+  }
 }
